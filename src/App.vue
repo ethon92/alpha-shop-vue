97879018a4fc6dfddf795e303f575acf1912b617
ethon92 <e19920526@gmail.com>
2022-03-22 17:19:41 +0800

    delete unnecessary comment

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
     <MainLayout>
       <main>
         <router-view />
-        <!-- <router-view @pass-shipping-fee="getFee" /> -->
-        <!-- <ShoppingCart :initial-shipping-fee="initialShippingFee"/> -->
       </main>
     </MainLayout>
   </div>
@@ -15,28 +13,14 @@ import MainLayout from './components/MainLayout.vue'
 
 export default {
   components: {
-    // ShoppingCart,
     MainLayout
   },
-  methods: {
-    // 將從shipping-form傳出的shipping-fee放入initialShippingFee
-    getFee(shippingFee) {
-      this.initialShippingFee = shippingFee
-    }
-  },
-  data() {
-    return {
-      initialShippingFee: ''
-    }
-  }
 }
 </script>
 
 <style lang="scss">
   // 載入reset css
   @import './assets/scss/main.scss';
-  // 載入共用樣式設定
-  @import './assets/scss/shareStyle.scss';
 
   // 各種form與shopping-cart的排版
   main {
